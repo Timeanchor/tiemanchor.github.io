a9f268686237316d7049d1d40748704a28dfc80f
josephd069 <145512721+josephd069@users.noreply.github.com>
2023-10-29 19:24:39 -0400

    Modified the "See More" so that it turns into "See Less" when clicked

--- timeanchor.js
+++ timeanchor.js
@@ -1,4 +1,10 @@
-function showText(divId){
-    var feaText=document.getElementById(divId);
-    feaText.style.display="block";
+function showText(divId) {
+  var feaText = document.getElementById(divId);
+  if (feaText.style.display === "none" || feaText.style.display === "") {
+    feaText.style.display = "block";
+    document.getElementById(divId + "-button").textContent = "See Less";
+  } else {
+    feaText.style.display = "none";
+    document.getElementById(divId + "-button").textContent = "See More";
+  }
 }
